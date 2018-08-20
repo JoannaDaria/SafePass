259166d46e4f59bb2d7d1cc7cbb9c1342e07c40f
Daria Kuczmara <dkuczmara@gmail.com>
2018-08-20 17:00:44 +0200

    Add future/application-scaffold branch

---
+++ src/test/java/passGenTest.java
@@ -0,0 +1,16 @@
+import generators.PasswordGenerator;
+import org.assertj.core.api.Assertions;
+import org.junit.Test;
+
+public class passGenTest {
+
+    @Test
+    public void passGenGeneratesStringOfGivenLength(){
+        Assertions.assertThat(PasswordGenerator.passGen(100).length()).isEqualTo(100);
+        Assertions.assertThat(PasswordGenerator.passGen(7).length()).isEqualTo(7);
+        Assertions.assertThat(PasswordGenerator.passGen(1).length()).isEqualTo(1);
+        Assertions.assertThat(PasswordGenerator.passGen(0).length()).isEqualTo(0);
+    }
+
+
+}
